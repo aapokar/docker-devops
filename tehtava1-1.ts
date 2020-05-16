b9f4e60066c460cdd36dde9e88e1e81d0ae68e23
Aapo Kärki <aapok@msn.com>
2020-05-16 18:34:54 +0300

    t1.1 & 1.2

---
+++ tehtava1-1.ts
@@ -0,0 +1,12 @@
+Script started on Sat 16 May 2020 10:56:59 UTC
+]0;pi@akpi: ~[01;32mpi@akpi[00m:[01;34m~ $[00m cat te[K[K[K[K[K[Kdocker ps -ascript tehtava1-1.ts[C[C[C[C[C[C[C[C[C[C[C[C[8Pdocker ps -als[Kcat typescript[C[C[C[C[C[C[C[C[C[C[C[Cls[Kdocker ps -ascript tehtava1-1.ts[C[C[C[C[C[C[C[C[C[C[C[C[8Pdocker ps -a[Kcat tehtava1-1,.[K[K.ts
+Script started on Sat 16 May 2020 10:56:59 UTC
+]0;pi@akpi: ~[01;32mpi@akpi[00m:[01;34m~ $[00m cat te[K[K[K[K[K[Kdocker ps -ascript tehtava1-1.ts[C[C[C[C[C[C[C[C[C[C[C[C[8Pdocker ps -als[Kcat typescript[C[C[C[C[C[C[C[C[C[C[C[Cls[Kdocker ps -ascript tehtava1-1.ts[C[C[C[C[C[C[C[C[C[C[C[C[8Pdocker ps -a[Kcat tehtava1-1,.[K[K.ts
+]0;pi@akpi: ~[01;32mpi@akpi[00m:[01;34m~ $[00m ls[K[Kdocker ps -a
+CONTAINER ID        IMAGE               COMMAND                  CREATED             STATUS                        PORTS               NAMES
+c9730be92cc3        nginx               "nginx -g 'daemon of…"   19 minutes ago      Exited (0) 16 minutes ago                         gifted_pike
+68716b732fc6        ubuntu:16.04        "sh -c 'while true; …"   21 minutes ago      Exited (137) 15 minutes ago                       looper
+d3548228c0bf        nginx               "nginx -g 'daemon of…"   31 minutes ago      Up 31 minutes                 80/tcp              charming_fermat
+]0;pi@akpi: ~[01;32mpi@akpi[00m:[01;34m~ $[00m docker ps -acat tehtava1-1.ts
+Script started on Sat 16 May 2020 10:56:59 UTC
+]0;pi@akpi: ~[01;32mpi@akpi[00m:[01;34m~ $[00m cat te[K[K[K[K[K[Kdocker ps -ascript tehtava1-1.ts[C[C[C[C[C[C[C[C[C[C[C[C[8Pdocker ps -als[Kcat typescript[C[C[C[C[C[C[C[C[C[C[C[Cls[Kdocker ps -ascript tehtava1-1.ts[C[C[C[C[C[C[C[C[C[C[C[C[8Pdocker ps -a[Kcat tehtava1-1,.[K[K.ts
